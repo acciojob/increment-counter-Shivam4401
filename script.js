61ec12dcd4fcd9e778308c42a3dcaa6eeb840cc2
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-02 16:48:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let val = document.querySelector("#counter");
+let btn = document.querySelector("#incrementBtn");
+
+let count = 0;
+
+function counterValue () {
+	alert(`localhost:3000 says
+	${count}`);
+	count++;
+	val.textContent = count;
+}
+
+btn.addEventListener("click",counterValue);
+
